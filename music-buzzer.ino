e925f94c316c165de36bff124f7912ccf2a4b987
soytw <chengkongtw@gmail.com>
2016-11-18 11:44:18 +0800

    Update music-buzzer.ino

--- music-buzzer.ino
+++ music-buzzer.ino
@@ -7,32 +7,41 @@ const int sharpfreq[] = {554,622,0,740,831,932,0};
 const int highfreq[] = {1047,1175,1319,1397,1568,1760,1976};
 const int highsharpfreq[] = {1109,1245,0,1480,1661,1865,0};
 
+struct Note {
+    int pitch, value;
+};
+
 int bpm = 480;
-char melody[] = "6234+562275671#^2^2256+54+34+5432+343+212";
+char melody[] = "6234#^562275671#^2^2256#^54#^34#^5432343#^212";
 // #: sharp
 // ^: high 8 degree
 // +: # and ^
 char note[] = "21111222211112222111121111211113";
 
+int fromstr(char melody[], char tempo[], Note res[]) {
+    int n = strlen(melody);
+    int i = 0;
+    int len = 0;
+    while (i < n) {
+        if (melody[i+1]=='#' && melody[i+2]=='^')
+            res[len].pitch = sharpfreq[melody[i]-'1'];
+        else if (melody[i+1]=='^')
+            res[len].pitch = highfreq[melody[i]-'1'];
+        else
+            res[len].pitch = freq[melody[i]-'1'];
+        res[len].value = tempo[len]-'0';
+        len++;
+        while (melody[i] < '0' || melody[i] > '9') i++;
+    }
+}
 void setup() {
     pinMode(buzz,OUTPUT);
 }
 void loop() {
-    int n = strlen(melody);
-    int i = 0;
-    while (i < n) {
-        double dur = (note[i]-'0')*60000.0/bpm;
-        int f;
-        if (melody[i+1]=='#' && melody[i+2]=='^')
-            f = sharpfreq[melody[i]-'1'];
-        else if (melody[i+1]=='^')
-            f = highfreq[melody[i]-'1'];
-        else
-            f = freq[melody[i]-'1'];
-        tone(buzz, f, dur);
-        delay(dur*1.3);
-        noTone(buzz);
-        while (note[i] < '0' || note[i] > '9') i++;
-    }
+    Note 
+        // double dur = (note[len]-'0')*60000.0/bpm;
+        // tone(buzz, f, dur);
+        // delay(dur*1.3);
+        // noTone(buzz);
     delay(1000);
 }
