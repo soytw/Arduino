ce70e9287b62497878dfab5a97ee9c9700f42909
soytw <soytwww@gmail.com>
2017-01-02 22:21:10 +0800

    finish octave 5 (piano sound)\nmanually type musicsheet in monotor

--- musicbox/music.h
+++ musicbox/music.h
@@ -6,7 +6,9 @@
 
 namespace Music {
     struct Note {
-        double freq, dur;
+        int8_t pitch, octave, sharp;
+        int freq;
+        long dur;
     };
     double frequency(int pitch, int octave, int sharp=0) {
         /**
@@ -14,13 +16,17 @@ namespace Music {
          * the pitch should be described in scientific pitch notation
          * for example, "D5#" of C tone get be described as (2,5,1)
          * @param pitch 1-7 note pitch in numeric notation
+         *              or 0 if it is a rest
          * @param octave 0-10
          * @param sharp sharp if positive, flat if negative
          * @return the frequency the pitch represent, in Hertz
+         *         0 when rest
          */
-        int mp[] = { 0,60,62,64,65,67,69,71 };
-        double p = mp[pitch] + sharp + (octave - 4) * 12;
-        return pow(2, (p - 69) / 12) * 440;
+        if (pitch == 0) return 0;
+        // MIDI's standard
+        int mp[] = {0,60,62,64,65,67,69,71};
+        int p = mp[pitch] + sharp + (octave-4)*12;
+        return pow(2, (p-69)/12.0) * 440;
     }
     double duration(int notelen, int bpm) {
         /**
@@ -31,7 +37,7 @@ namespace Music {
          */
         return notelen * 60000.0 / bpm;
     }
-    int fromstr(const char src[], Note res[], int bpm, int sharp=0) {
+    size_t fromstr(const char src[], Note res[], int bpm, int sharp=0) {
         /**
          * translate numeric music notation into frequency and duration
          * example: 5-1234 5-1-1- 6-4567 1^-1-1- 4-5432 3-4321 2-3217.1-
@@ -46,10 +52,11 @@ namespace Music {
          * @param sharp used to change mode
          * @return length of res array
          */
-        int srclen = strlen(src);
-        int reslen = 0;
-        for (int i = 0; i<srclen; ) {
+        size_t srclen = strlen(src);
+        size_t reslen = 0;
+        for (size_t i=0; i<srclen; ) {
             int pitch = src[i] - '0';
+            if (pitch<1 || pitch>7) pitch = 0;
             int octave = 5;
             int notelen = 1;
             i++;
@@ -66,6 +73,9 @@ namespace Music {
                     notelen++;
                 i++;
             }
+            res[reslen].pitch = pitch;
+            res[reslen].octave = octave;
+            res[reslen].sharp = sharp;
             res[reslen].freq = frequency(pitch, octave, sharp);
             res[reslen].dur = duration(notelen, bpm);
             reslen++;
--- musicbox/musicbox.ino
+++ musicbox/musicbox.ino
@@ -25,35 +25,41 @@
 
  created  29 Jun 2012 by Lutz Lisseck
  */
+#include <stdio.h>
 #include <SimpleSDAudio.h>
 #include "music.h"
+
+
+#define btn0 2
+bool play;
+
+#define MAX_MUSICSHEET_LEN 50
+char musicsheet[MAX_MUSICSHEET_LEN];
+Music::Note notes[MAX_MUSICSHEET_LEN];
+
+// Create static buffer
+// bigger than 2*512 is often only possible on Arduino megas!
+#define BIGBUFSIZE (2*512)
+uint8_t bigbuf[BIGBUFSIZE];
+
 
 // Callback target, prints output to serial
 void DirCallback(char *buf) {
   Serial.println(buf);
 }
 
-char musicsheet[20];
-Music::Note notes[20];
-
-// Create static buffer
-#define BIGBUFSIZE (2*512)      // bigger than 2*512 is often only possible on Arduino megas!
-uint8_t bigbuf[BIGBUFSIZE];
-
 // helper function to determine free ram at runtime
-int freeRam () {
+int freeRam() {
   extern int __heap_start, *__brkval;
   int v;
   return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
 }
 
-void setup()
-{
- // Open serial communications and wait for port to open:
+void setup() {
+  // Open serial communications and wait for port to open:
   Serial.begin(9600);
-   while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
+  // wait for serial port to connect. Needed for Leonardo only
+  while (!Serial);
 
   Serial.print(F("Free Ram: "));
   Serial.println(freeRam());
@@ -80,29 +86,74 @@ void setup()
     Serial.println(F("Wiring is correct and a card is present."));
   }
 
-
+  pinMode(btn0, INPUT);
+  play = 1;
 }
 
-void loop(void) {
+void loop() {
   uint8_t count=0, c, flag;
 
   Serial.println(F("Files on card:"));
   SdPlay.dir(&DirCallback);
 
-ReEnter:
-  count = 0;
-  Serial.println(F("\r\nEnter music sheet (send LF after line):"));
-  do {
-    while(!Serial.available()) ;
-    c = Serial.read();
-    if(c > ' ') musicsheet[count++] = c;
-  } while((c != 0x0d) && (c != 0x0a) && (count < 14));
-  musicsheet[count++] = 0;
+  if (digitalRead(btn0) == HIGH) {
+  	play = !play;
+  	Serial.println("start/pause");
+  	delay(100);
+  }
+  if (!play) return;
 
-  Music::fromstr(musicsheet,notes,240);
+ReEnter:
+  // count = 0;
+  // Serial.println(F("\r\nEnter music sheet (20 characters at most) (send LF after line):"));
+  // do {
+  //   while (!Serial.available());
+  //   c = Serial.read();
+  //   if (c > ' ') musicsheet[count++] = c;
+  // } while (c != '\n' && c != '\r' && count < MAX_MUSICSHEET_LEN);
+  // musicsheet[count++] = 0;
+
+  // uint8_t len = Music::fromstr(musicsheet,notes,240);
+  uint8_t len = Music::fromstr("55552-0055552-00555554567-5-2-",notes,240); // test
+
+  // Serial.print("You entered: ");
+  // Serial.println(musicsheet);
+
+  // each pitch is at most 1.6 seconds long
+  // so the minimum BPM = 37.5
+  char notename[] = " CDEFGAB";
+  for (int i=0; i<len; i++) {
+    if (notes[i].pitch != 0) {
+      char filename[10];
+      if (notes[i].sharp)
+        sprintf(filename, "%c%ds.afm", notename[notes[i].pitch], notes[i].octave);
+      else
+        sprintf(filename, "%c%d.afm", notename[notes[i].pitch], notes[i].octave);
+      bool result = SdPlay.setFile(filename);
+
+      // DEBUG
+      Serial.print("Playing: ");
+      Serial.println(filename);
+      if (result)
+        Serial.println("Found");
+      else {
+        Serial.print("Error: ");
+        Serial.println(SdPlay.getLastError());
+      }
+      // END DEBUG
+
+      SdPlay.play();
+    }
+    unsigned long starttime = millis();
+    while (millis()-starttime <= notes[i].dur) {
+      SdPlay.worker();
+    }
+  }
+
+  delay(1000);
 
   // Serial.print(F("Looking for file... "));
-  // if(!SdPlay.setFile(musicsheet)) {
+  // if(!SdPlay.setFile("default.afm")) {
   //   Serial.println(F(" not found on card! Error code: "));
   //   Serial.println(SdPlay.getLastError());
   //   goto ReEnter;
@@ -110,59 +161,59 @@ ReEnter:
   //  Serial.println(F("found."));
   // }
 
-  Serial.println(F("s: stop"));
-  Serial.println(F("p: play"));
-  Serial.println(F("h: pause"));
-  Serial.println(F("f: reenter sheet"));
-  Serial.println(F("d: unmount SD"));
-  Serial.println(F("v: view status"));
-  flag = 1;
+  // Serial.println(F("s: stop"));
+  // Serial.println(F("p: play"));
+  // Serial.println(F("h: pause"));
+  // Serial.println(F("f: reenter sheet"));
+  // Serial.println(F("d: unmount SD"));
+  // Serial.println(F("v: view status"));
+  // flag = 1;
 
-  while(flag) {
-    SdPlay.worker();
-    if(Serial.available()) {
-      c = Serial.read();
-      switch(c) {
-         case 's':
-           SdPlay.stop();
-           Serial.println(F("Stopped."));
-           break;
+  // while(flag) {
+  //   SdPlay.worker();
+  //   if(Serial.available()) {
+  //     c = Serial.read();
+  //     switch(c) {
+  //        case 's':
+  //          SdPlay.stop();
+  //          Serial.println(F("Stopped."));
+  //          break;
 
-         case 'p':
-           SdPlay.play();
-           Serial.println(F("Play."));
-           break;
+  //        case 'p':
+  //          SdPlay.play();
+  //          Serial.println(F("Play."));
+  //          break;
 
-         case 'h':
-           SdPlay.pause();
-           Serial.println(F("Pause."));
-           break;
+  //        case 'h':
+  //          SdPlay.pause();
+  //          Serial.println(F("Pause."));
+  //          break;
 
-         case 'd':
-           SdPlay.deInit();
-           Serial.println(F("SdPlay deinitialized. You can now safely remove card. System halted."));
-           while(1) ;
-           break;
+  //        case 'd':
+  //          SdPlay.deInit();
+  //          Serial.println(F("SdPlay deinitialized. You can now safely remove card. System halted."));
+  //          while(1) ;
+  //          break;
 
-         case 'f':
-           flag = 0;
-           break;
+  //        case 'f':
+  //          flag = 0;
+  //          break;
 
-         case 'v':
-           Serial.print(F("Status: isStopped="));
-           Serial.print(SdPlay.isStopped());
-           Serial.print(F(", isPlaying="));
-           Serial.print(SdPlay.isPlaying());
-           Serial.print(F(", isPaused="));
-           Serial.print(SdPlay.isPaused());
-           Serial.print(F(", isUnderrunOccured="));
-           Serial.print(SdPlay.isUnderrunOccured());
-           Serial.print(F(", getLastError="));
-           Serial.println(SdPlay.getLastError());
-           Serial.print(F("Free RAM: "));
-           Serial.println(freeRam());
-           break;
-      }
-    }
-  }
+  //        case 'v':
+  //          Serial.print(F("Status: isStopped="));
+  //          Serial.print(SdPlay.isStopped());
+  //          Serial.print(F(", isPlaying="));
+  //          Serial.print(SdPlay.isPlaying());
+  //          Serial.print(F(", isPaused="));
+  //          Serial.print(SdPlay.isPaused());
+  //          Serial.print(F(", isUnderrunOccured="));
+  //          Serial.print(SdPlay.isUnderrunOccured());
+  //          Serial.print(F(", getLastError="));
+  //          Serial.println(SdPlay.getLastError());
+  //          Serial.print(F("Free RAM: "));
+  //          Serial.println(freeRam());
+  //          break;
+  //     }
+  //   }
+  // }
 }
