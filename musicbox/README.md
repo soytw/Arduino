087865d4d690d83b9c01f334ae3156724945981c
soytw <soytwww@gmail.com>
2017-01-08 18:20:15 +0800

    using violin sound

--- musicbox/README.md
+++ musicbox/README.md
@@ -12,6 +12,7 @@
 - [ ] Light effects
 
 # PINS
+- Volume a5
 - CS 4
 - Speaker 9
 - MOSI 11
@@ -19,21 +20,21 @@
 - SCK 13
 
 # BUTTONS
-- btn0 d2 a0 C
-- btn1 d2 a1 C#
-- btn2 d2 a2 D
-- btn3 d2 a3 D#
-- btn4 d3 a0 E
-- btn5 d3 a1 F
-- btn6 d3 a2 F#
-- btn7 d3 a3 G
-- btn8 d5 a0 G#
-- btn9 d5 a1 A
-- btn10 d5 a2 A#
-- btn11 d5 a3 B
-- btn12 d6 a0
-- btn13 d6 a1
-- btn14 d6 a2
-- btn15 d6 a3
+- btn1 d2 a0 C
+- btn2 d2 a1 C#
+- btn3 d2 a2 D
+- btn4 d2 a3 D#
+- btn5 d3 a0 E
+- btn6 d3 a1 F
+- btn7 d3 a2 F#
+- btn8 d3 a3 G
+- btn9 d5 a0 G#
+- btn10 d5 a1 A
+- btn11 d5 a2 A#
+- btn12 d5 a3 B
+- btn13 d6 a0 play/pause
+- btn14 d6 a1 record
+- btn15 d6 a2 next instrument
+- btn16 d6 a3
 
 https://create.arduino.cc/projecthub/lindsi8784/electronic-piano-keyboard-with-preset-songs-74ee7c
--- musicbox/musicbox.ino
+++ musicbox/musicbox.ino
@@ -1,8 +1,11 @@
-#include <stdio.h>
 #include <TMRpcm.h>
 #include <Keypad.h>
-#include "music.h"
 
+#define debug
+
+#define VOL_PIN A4
+#define SD_PIN 4
+#define SPEAKER_PIN 9
 
 byte keys[4][4] = {
     { 1,  2,  3,  4},
@@ -14,9 +17,9 @@ byte rowPin[4] = {2, 3, 5, 6};
 byte colPin[4] = {A0, A1, A2, A3};
 Keypad keypad = Keypad(makeKeymap(keys), rowPin, colPin, 4, 4);
 
-#define MAX_MUSICSHEET_LEN 50
-char musicsheet[MAX_MUSICSHEET_LEN];
-Music::Note notes[MAX_MUSICSHEET_LEN];
+// #define MAX_MUSICSHEET_LEN 50
+// char musicsheet[MAX_MUSICSHEET_LEN];
+// Music::Note notes[MAX_MUSICSHEET_LEN];
 
 TMRpcm audio;
 File sd;
@@ -59,28 +62,42 @@ void setup() {
   Serial.println(freeRam());
 
   Serial.println(F("Initializing SD Card"));
-  if (SD.begin(4)) {
+  if (SD.begin(SD_PIN)) {
     Serial.println("Success");
     Serial.println("Files:");
     sd = SD.open("/");
     printDir(sd);
-    audio.speakerPin = 9;
+    audio.speakerPin = SPEAKER_PIN;
   } else {
     Serial.print("SD fail");
   }
+
+  pinMode(VOL_PIN, INPUT);
 }
 
 void loop() {
+  // Serial.println("0 1 2 3 4 5 6 7 8 9 0 1 2 3");
+  // for (int i=0; i<=13; i++) {
+  //   Serial.print(' ');
+  //   Serial.print(digitalRead(i));
+  // }
+  // Serial.println();
+
+  audio.setVolume(map(analogRead(VOL_PIN), 0, 1023, 0, 7));
+
   byte key = keypad.getKey();
   if (key) Serial.println(key);
 
   if (key >= 1 && key <= 12) {
     // piano key
-    String notename[] = {"", "C5", "C5S", "D5", "D5S", "E5", "F5", "F5S", "G5", "G5S", "A5", "A5S", "B"};
-    String type = "PIANO";
+    String notename[] = {"", "C5", "C5S", "D5", "D5S", "E5", "F5", "F5S", "G5", "G5S", "A5", "A5S", "B5"};
+    String type = "violin";
     String filename = type + "/" + notename[key] + ".wav";
-    audio.play(filename.c_str());
     Serial.println(filename);
+    audio.play(filename.c_str());
+    unsigned long tm = millis();
+    while (millis() - tm <= 800) audio.loop(1);
+    audio.pause();
   } else if (key == 13) {
     // play / pause
   } else if (key == 14) {
@@ -89,5 +106,9 @@ void loop() {
     // next sound
   } else if (key == 16) {
     // ??
+  } else {
+    // no key pressed
+    // Serial.println("pause");
+    // audio.pause();
   }
 }
